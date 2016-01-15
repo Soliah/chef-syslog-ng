1e69c1249b782c1935dda8a4c1ad68642a022520
Christopher Chow <chris@chowie.net>
2016-01-16 00:26:22 +1100

    Rename to program instead of tag.

--- providers/papertrail_log_program.rb
+++ providers/papertrail_log_program.rb
@@ -1,13 +1,11 @@
 use_inline_resources
 
 action :create do
-  tags = new_resource.tags.map { |t| "\"#{t}\"" }.join(", ")
-
   template "/etc/syslog-ng/conf.d/#{new_resource.priority}-papertrail-#{new_resource.name}.conf" do
     cookbook "syslog-ng"
     mode 0664
-    source "papertrail/log_tag.conf.erb"
-    variables(destination: new_resource.destination, name: new_resource.name, tags: tags)
+    source "papertrail/log_program.conf.erb"
+    variables(destination: new_resource.destination, name: new_resource.name, program: new_resource.program)
   end
 end
 
--- resources/papertrail_log_program.rb
+++ resources/papertrail_log_program.rb
@@ -4,4 +4,4 @@ default_action :create
 property :name, kind_of: String, name_attribute: true, required: true
 property :priority, kind_of: Integer, required: true
 property :destination, kind_of: String, required: true
-property :tags, kind_of: Array, required: true, default: []
+property :program, kind_of: String, required: true, default: []
